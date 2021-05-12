724422fcb3b8006eb64858c596198cf5a437f5f0
dennis281 <61270407+dennis281@users.noreply.github.com>
2021-05-12 15:14:11 +0530

    aamarv.21

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+
+function footer(){
+    return (
+        <div className="footer">
+            
+            <div className="box mar">
+                <img src="http://onlinemeatshop.in/image/catalog/blog/chicken%20(3).png"></img>
+            </div>
+            <div className="boxes mar">
+                <h2>EXPERTISE</h2>
+                <p>Imports&Exports</p>
+                <p>FarmBreeding</p>
+                <p>Wholesale Products</p>
+                <p>Imports&Exports</p>
+                <p>FarmBreeding</p>
+                <p>Wholesale Products</p>
+
+            </div>
+            <div className="bosses mar">
+                <h2>COMPANY</h2>
+                <p>About Us</p>
+                <p>Careers</p>
+                <p>Terms of Use</p>
+                <p>contact us</p>
+                
+            </div>
+            <div className="bases mar">
+                <h2>LOCATION</h2>
+                <p>Chennai</p>
+                <p>Bangalore</p>
+                <p>Mumbai</p>
+                <p>Kolkata</p>
+                <p>Delhi</p>
+                <p>Hyderabad</p>
+            </div>
+            
+        </div>
+    )
+}
+ 
+export default footer;
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+
+function about() {
+
+                
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h1>ABOUT US </h1>
+            <p>loubgjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</p>
+        </nav>
+    )
+}
+
+export default about
